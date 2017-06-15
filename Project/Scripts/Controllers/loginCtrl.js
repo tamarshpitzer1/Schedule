c9919aa969275b104ed63233443a78b61b552e8b
hitmachut <hitmachut@TUT-N23>
2017-06-15 12:13:29 +0300

    Add project files.

---
+++ Project/Scripts/Controllers/loginCtrl.js
@@ -0,0 +1,35 @@
+﻿var app = angular.module('app', ['react']);
+
+app.controller('mainCtrl', ['$scope', '$window', function ($scope, $window) {
+    $scope.person = { fname: '', pass: '' };
+    $scope.worker = [{ name: "tami", pass: "123" }, { name: "tami", pass: "123" }];
+    $scope.cheak = function () {
+        for (var i = 0; i < $scope.worker.length; i++) {
+            if ($scope.worker[i].name == $scope.person.fname && $scope.worker[i].pass == $scope.person.pass) {
+                $window.location.href = 'homePage.html';
+            }
+        }
+    }
+
+
+}]);
+
+var Hello = React.createClass({
+    propTypes: {
+        fname: React.PropTypes.string.isRequired,
+        pass: React.PropTypes.string.isRequired
+    },
+
+
+    render: function () {
+        return React.DOM.span(null,
+          'Hello ' + this.props.fname
+        );
+    }
+});
+
+app.value("Hello", Hello);
+
+app.directive('hello', function (reactDirective) {
+    return reactDirective(Hello);
+});
---
+++ Project/Scripts/app.js
@@ -0,0 +1,22 @@
+﻿//--------------------------pages routing-----------------------------
+
+var myApp = angular.module('myApp', ['ui.bootstrap', 'ngRoute', 'ds.clock', 'ngAnimate', 'chart.js']);
+myApp.config(function ($RouteProvider) {
+    $RouteProvider
+        .when('/TimeTable', {
+            templateUrl: 'Scripts/TimeTable.html',
+            controller: 'timeTableCtrl'
+        })
+        .when('/HomePage', {
+            templateUrl: '../homePage.html',
+            controller: 'myCtrl'
+        })
+         .when('/EnterTime', {
+             templateUrl: '../EnterTime.html',
+             controller: 'myCtrl'
+         })
+        .when('/ExitTime', {
+            templateUrl: '../ExitTime.html',
+            controller: 'myCtrl'
+        })
+});
